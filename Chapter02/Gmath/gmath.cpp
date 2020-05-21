c166b78054049ca9caeb14c37c69c3a5ef395277
staralloff <18667850768@163.com>
2020-05-21 20:41:12 +0800

    Craps confused about how to compile the interface and the compilation order.

---
+++ Chapter02/Gmath/gmath.cpp
@@ -0,0 +1,28 @@
+/*
+ * File: gmath.cpp
+ * ---------------
+ * This file implements the gmath.h interface. In all case, the
+ * implementation for each function requires only one line of code,
+ * which makes detailed documentation unnecessary.
+ */
+
+#include <cmath>
+
+#include "gmath.h"
+extern const double PI = 3.14159265358979323846;
+
+double sinDegrees(double angle) {
+    return sin(toRadians(angle));
+}
+
+double cosDegrees(double angle) {
+    return cos(toRadians(angle));
+}
+
+double toDegrees(double radians) {
+    return radians * 180 / PI;
+}
+
+double toRadians(double degrees) {
+    return degrees * PI / 180;
+}
