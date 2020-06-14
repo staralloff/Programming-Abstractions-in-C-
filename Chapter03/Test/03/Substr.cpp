89e99bfb635ac792f7f803c88ee038556f612355
staralloff <18667850768@163.com>
2020-06-14 12:37:21 +0800

    substr without strlib

--- Chapter03/Test/03/Substr.cpp
+++ Chapter03/Test/03/Substr.cpp
@@ -1,2 +1,43 @@
+/*
+ * File: Substr.cpp
+ * ----------------
+ * This program implements substr method without strlib
+ * return the  length N sub string from position pos.
+ * Note:
+ * 1.if miss n or n bigger than str.length, the sub string should ends
+ * at the tail of str.
+ * 2.if pos bigger than str.length, print the error msg.
+ */
 #include <iostream>
+#include <string>
 using namespace std;
+
+/* Function prototypes */
+
+void error(string msg);
+string substr(string str, int pos, int n);
+
+int main() {
+    string s;
+    int position, len;
+    cout << "Enter the string and sub string's start position and sub string's length: " << endl;
+    getline(cin, s);
+    cin >> position >> len;
+    cout << substr(s, position, len) << endl;
+    return 0;
+}
+
+string substr(string str, int pos, int n) {
+    if(pos > str.length()) error("pos is out of string's length");
+    string rev = "";
+    int end = (n==0 ? str.length() : pos + n);
+    for(int i = pos; i < end && i < str.length(); i++) {
+        rev += str.at(i);
+    }
+    return rev;
+}
+
+void error(string msg) {
+    cerr << msg << endl;
+    exit(EXIT_FAILURE);
+}
